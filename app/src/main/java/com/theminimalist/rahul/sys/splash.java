08dd5f2524ea05e42fbf2aca1726981002a13a5f
cronaldo <cronaldoin7@gmail.com>
2017-09-08 17:23:11 +0530

    RUC created for login and register

--- app/src/main/java/com/theminimalist/rahul/sys/splash.java
+++ app/src/main/java/com/theminimalist/rahul/sys/splash.java
@@ -17,19 +17,14 @@ public class splash extends AppCompatActivity {
         setContentView(R.layout.splash);
         new Handler().postDelayed(new Runnable() {
 
-            /*
-                 * Showing splash screen with a timer. This will be useful when you
-                 * want to show case your app logo / company
-                 */
             @Override
             public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
+
                 Intent go = new Intent(splash.this,Login.class);
                 startActivity(go);
 
 
-                // close this activity
+
                 finish();
             }
         }, SPLASH_TIME_OUT);
