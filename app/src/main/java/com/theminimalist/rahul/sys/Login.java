11bbbc9696f944e8be3301fb0b121baf47de6fe5
cronaldo <cronaldoin7@gmail.com>
2017-09-08 16:43:07 +0530

    splash added

--- app/src/main/java/com/theminimalist/rahul/sys/Login.java
+++ app/src/main/java/com/theminimalist/rahul/sys/Login.java
@@ -3,11 +3,11 @@ package com.theminimalist.rahul.sys;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Intro_Activity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro_);
+        setContentView(R.layout.activity_login);
     }
 }
---
+++ app/src/main/java/com/theminimalist/rahul/sys/splash.java
@@ -0,0 +1,43 @@
+package com.theminimalist.rahul.sys;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
+
+public class splash extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 2000;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+                 * Showing splash screen with a timer. This will be useful when you
+                 * want to show case your app logo / company
+                 */
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent go = new Intent(splash.this,Login.class);
+                startActivity(go);
+
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+
+
+
+
+
+}
